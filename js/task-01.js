a62b0f0f2d117c0b7a7e99935af007adbd7e2a09
Vladislav Virchenko <virchenko.vlad.2011@icloud.com>
2023-01-10 22:08:26 +0200

    task2

--- js/task-01.js
+++ js/task-01.js
@@ -3,7 +3,6 @@ const categoriesEl = document.querySelectorAll('.item');
 console.log(`Number of categories: ${categoriesEl.length}`)
 
 const categoriesDetail = categoriesEl.forEach((category) => {
-
     console.log(`Category: ${category.firstElementChild.textContent}`);
     console.log(`Elements: ${category.querySelectorAll('li').length}`)
 });
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,27 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+// Створить окремий елемент <li>. 
+// Обов'язково використовуй метод document.createElement().
+
+// Додасть назву інгредієнта як його текстовий вміст.
+
+// Додасть елементу клас item.
+
+// Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+
+const ingredientsEl = document.querySelector('#ingredients');
+console.log(ingredientsEl);
+
+const ingredientListEl = ingredients.map((ingridient) => {
+  const ingredientEl = document.createElement('li');
+  ingredientEl.classList.add('item');
+  ingredientEl.textContent = ingridient;
+  // console.log(ingredientEl);
+  return ingredientEl;
+});
+
+
+ingredientsEl.append(...ingredientListEl);
