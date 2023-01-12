440aeb67ef537fcfd8c186da5c0544453e139705
Vladislav Virchenko <virchenko.vlad.2011@icloud.com>
2023-01-12 16:37:47 +0200

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+// console.log(getRandomHexColor());
+
+
+const body = document.querySelector('body');
+// console.log(body);
+const spanEl = document.querySelector('.color');
+const changeBtn = document.querySelector('.change-color');
+
+changeBtn.addEventListener('click', onBtnChange);
+
+function onBtnChange() {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  spanEl.textContent = randomColor;
+};
