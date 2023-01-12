7a9f3354b60aa7cc97b7998440adbdff964c6b19
Vladislav Virchenko <virchenko.vlad.2011@icloud.com>
2023-01-12 16:26:21 +0200

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector('.login-form');
+console.log(form.elements);
 
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const { elements: { email, password} } = event.currentTarget;
+    
+    if (email.value === '' || password.value === '') {
+        alert('Всі поля повинні бути заповнені!')
+    } else {
+        console.log({ 'email': email.value, 'password': password.value, 'name':name });
+        form.reset();
+   }
+}
