92e1b7ff9f2dd0ea88edc0bacfb711886f02a7c0
Vladislav Virchenko <virchenko.vlad.2011@icloud.com>
2023-01-13 18:16:45 +0200

    task1

--- js/task-01.js
+++ js/task-01.js
@@ -4,7 +4,7 @@ console.log(`Number of categories: ${categoriesEl.length}`)
 
 const categoriesDetail = categoriesEl.forEach((category) => {
     console.log(`Category: ${category.firstElementChild.textContent}`);
-    console.log(`Elements: ${category.querySelectorAll('li').length}`)
+    console.log(`Elements: ${category.lastElementChild.childElementCount}`)
 });
 
 
